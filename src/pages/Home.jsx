e00863d439b4cccb1c381663cd2a19e01b4cc53a
lirc572 <lirc572@gmail.com>
2022-01-03 15:25:42 +0800

    style: update home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+const linkStyle = {
+  marginLeft: '0.25rem',
+  color: '#595954',
+};
 
 export default () => {
   const history = useHistory();
@@ -19,21 +24,32 @@ export default () => {
         flexDirection: 'row',
         justifyContent: 'center',
       }}>
-        <h1>Resume</h1>
+        <h1>Ruochen's Resume</h1>
       </div>
       <div style={{
         marginTop: '2rem',
+        flex: 1,
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        gap: '8rem',
+        alignItems: 'center',
+        gap: '1rem',
       }}>
         <div className="link-div" onClick={() => history.push("/portrait")}>
-          <span to="/portrait">A4 Portrait</span>
+          <span>A4 Portrait</span>
         </div>
         <div className="link-div" onClick={() => history.push("/landscape")}>
           <span>A4 Landscape</span>
         </div>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0.5rem',
+      }}>
+        Built with <a style={linkStyle} href="https://reactjs.org/">React</a>, <a style={linkStyle} href="https://vitejs.dev/">Vite</a>, and <a style={linkStyle} href="https://react-pdf.org/">React-pdf</a>.
       </div>
     </div>
   );
